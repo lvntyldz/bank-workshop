005b9a527920707ff6e5129522d65cb57e9ccff1
levent yıldız <lewenty@gmail.com>
2020-10-02 17:00:17 +0300

    add springboot support to maven project

--- springboot-example/src/main/java/com/ba/Run.java
+++ springboot-example/src/main/java/com/ba/Run.java
@@ -1,7 +1,13 @@
 package com.ba;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Run {
+
     public static void main(String[] args) {
-        System.out.println("Hello BA");
+        SpringApplication.run(Run.class, args);
     }
+
 }
