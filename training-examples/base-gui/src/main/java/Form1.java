054cd3d573163e38f86c7461eb4f222cddf3a133
levent yıldız <levent.yildiz@hotmail.com.tr>
2020-11-09 16:22:02 +0300

    add base gui form data

--- training-examples/base-gui/src/main/java/Form1.java
+++ training-examples/base-gui/src/main/java/Form1.java
@@ -6,11 +6,26 @@ import java.awt.event.ActionListener;
 public class Form1 {
     private JButton button1;
     private JPanel mainPanel;
+    private JTextField searchTxt;
+    private JList resultList;
 
     public Form1() {
+
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        listModel.addElement("USA");
+        listModel.addElement("India");
+        listModel.addElement("Vietnam");
+        listModel.addElement("Canada");
+        listModel.addElement("Denmark");
+        listModel.addElement("France");
+        listModel.addElement("Great Britain");
+        listModel.addElement("Japan");
+        resultList.setModel(listModel);
+
         button1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,"Button Clicked");
+                String searchData = searchTxt.getText();
+                JOptionPane.showMessageDialog(null, "Button Clicked : " + searchData);
             }
         });
     }
